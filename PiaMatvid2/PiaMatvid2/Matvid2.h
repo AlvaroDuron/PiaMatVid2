338cce0d02b09ca445adbfe2e5e162fbe7bdc3ac
Alvaro Duron <alvaro_07051@outlook.com>
2020-11-20 21:57:33 -0600

    versioj final

---
+++ PiaMatvid2/PiaMatvid2/Matvid2.h
@@ -0,0 +1,13 @@
+#ifndef MATVID2_H
+#define MATVID2_H
+
+#include "Ventana.h"
+#include "PantallaMenu.h"
+#include "PantallaMatrices.h"
+#include "PantallaCuaterniones.h"
+#include "Defines.h"
+#include "PantallaTransformaciones.h"
+#include "PantallaVectores.h"
+#include "PantallaBresenham.h"
+
+#endif // !MATIVID2_H
